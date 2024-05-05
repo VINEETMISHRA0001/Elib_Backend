4840b0a57e135b14a13a80efff82267ce6a4f2ea
Vineet Mishra <02vineetmishra@gmail.com>
2024-05-05 11:43:54 +0530

    Global Eroor Handler Added

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import createHttpError, { HttpError } from 'http-errors';
+import { config } from './config/config';
+import { globalErrorHanlder } from './middlewares/globalErrorHandler';
 
 const app = express();
 
@@ -7,5 +10,9 @@ const app = express();
 app.get('/', (req, res, next) => {
   res.json({ message: 'Hello From the server' });
 });
+
+// global error handler (always put at the end)
+
+app.use(globalErrorHanlder);
 
 export default app;
