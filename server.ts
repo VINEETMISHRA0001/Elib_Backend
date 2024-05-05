e5c9152c6680ae3860fb1788c6effd9183cd91bc
Vineet Mishra <02vineetmishra@gmail.com>
2024-05-05 08:43:54 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
 // not setting up espress here , only setting up server in this file
 import app from './src/app';
+import { config } from './src/config/config';
 
 const startServer = () => {
-  const port = process.env.PORT || 3000;
+  const port = config.port || 3000;
 
   app.listen(port, () => {
     console.log(`Server is Listening on Port: ${port}`);
