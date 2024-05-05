2917871ff40a3d992427eb923a9cd63781d33acc
Vineet Mishra <02vineetmishra@gmail.com>
2024-05-05 08:29:50 +0530

    express server setup

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
-console.log(`Welocome to ebook Api's`);
+// not setting up espress here , only setting up server in this file
+import app from './src/app';
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Server is Listening on Port: ${port}`);
+  });
+};
+
+startServer();
