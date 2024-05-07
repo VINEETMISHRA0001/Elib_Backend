847ae21f3d570451a599fdc820371952fb0fdbd5
Vineet Mishra <02vineetmishra@gmail.com>
2024-05-07 22:54:49 +0530

    update book

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import express from 'express';
-import { createBook } from './bookController';
+import { createBook, updateBook } from './bookController';
 import multer from 'multer';
 import { authenticatedUser } from '../middlewares/authenticate';
 
@@ -19,6 +19,16 @@ bookRouter.post(
     { name: 'file', maxCount: 1 },
   ]),
   createBook
+);
+
+bookRouter.patch(
+  '/update/:id',
+  authenticatedUser,
+  upload.fields([
+    { name: 'coverImage', maxCount: 1 },
+    { name: 'file', maxCount: 1 },
+  ]),
+  updateBook
 );
 
 export default bookRouter;
